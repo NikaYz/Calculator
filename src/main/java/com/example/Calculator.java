cd16246371cc0bde4958724315522ec92a838d74
NikaYz <adisaraf4444@gmail.com>
2025-11-03 18:52:34 +0530

    For testing build using ngrok

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -3,7 +3,7 @@ package com.example;
 import java.util.Scanner;
 
 public class Calculator{
-    
+    // testing for live build using ngrok
     // sqrt function: do square root of x
     // Ex: sqrt(4) = 2.0
     // Ex: sqrt(-1) = Error
